e20eb7af481b71d1b07c95c4669d011e631320e3
dashkevics <dashkevics@gmail.com>
2020-10-22 21:15:55 +0600

    portfoio page

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
+
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
+
+buttonRight.onclick = function () {
+  document.getElementById('overflow').scrollLeft += 350;
+};
+buttonLeft.onclick = function () {
+  document.getElementById('overflow').scrollLeft -= 350;
+};

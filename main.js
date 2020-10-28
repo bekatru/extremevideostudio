43187e1073bb3edb3a212c9d92756abed71eee13
Beka <teamusicband@gmail.com>
2020-10-28 22:49:09 +0300

    carousel added

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-let navbar = document.getElementById("navigations");
-let sticky = navbar.offsetTop;
+// let navbar = document.getElementById("navigations");
+// let sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 function togglePopup(){
   document.getElementById("popup").classList.toggle("active");
